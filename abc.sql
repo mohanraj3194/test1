5280dbf228d1cb90eef7e5545e76f217813149a2
mohanraj3194 <mohanrajrselvaraj@gmail.com>
2024-10-11 10:40:53 +0530

    one row inserted in 105_id

--- abc.sql
+++ abc.sql
@@ -3,3 +3,4 @@ insert into test values (106, 'mohan');
 update test set first_name = 'abi' where employee_id = 10;
 commit;
 delete from test where employee_id = 105;
+insert into test values (105, 'ajith');
